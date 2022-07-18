3f4e9a5b2e1ff19b1d53e2515cad0cdd2330c458
Angga <emmanuelangga@gmail.com>
2022-07-18 16:07:25 +0700

    ENV Mode update

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { routes } = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: 'localhost', //process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
           cors: {
             origin: ['*'],
@@ -19,3 +19,9 @@ const init = async () => {
 };
 
 init();
+
+/*
+Ubah script run di package.json menjadi:
+"start-prod": "NODE_ENV=production node ./src/server.js"
+"start-dev": "nodemon ./src/server.js",
+*/
